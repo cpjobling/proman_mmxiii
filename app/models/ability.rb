a0afc4d0058e3162d6b11ffa9008048f8938310f
Chris Jobling <c.p.jobling@swansea.ac.uk>
2012-08-14 19:46:45 +0100

    Add guest abilities for Project

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
+
+    if user.guest?
+        can :read, Project
+    else
+
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
