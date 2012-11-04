7461f99868267d19fae6bef1bf3ad82b26fdfe4d
Chris Jobling <c.p.jobling@swansea.ac.uk>
2012-11-04 00:06:38 +0000

    Simple routes for static pages and app root to static_pages#home.

--- spec/requests/viewing_projects_spec.rb
+++ spec/requests/viewing_projects_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature "Viewing Projects" do
   scenario "Listing projects" do
     project = FactoryGirl.create(:project, :title => "Build a gizmo")
-    visit "/"
+    visit projects_path
     click_link 'Build a gizmo'
 
     page.current_url.should == project_url(project)
