fecb98c36b11f52e6324d81ff85f134801ceb2c0
Chris Jobling <c.p.jobling@swansea.ac.uk>
2012-11-04 14:00:36 +0000

    Add user_pages_spec.rb

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit new_user_registration_path }
+
+    page!
+
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+  end
+end
